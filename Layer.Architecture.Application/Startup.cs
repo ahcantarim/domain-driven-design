4c0849d40cf6717ec427940a34f5d974f0b90879
ahcantarim <andre.cantarim@gmail.com>
2021-03-16 12:53:01 -0300

    Estrutura finalizada (sem Migration)

--- Layer.Architecture.Application/Startup.cs
+++ Layer.Architecture.Application/Startup.cs
@@ -2,10 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Layer.Architecture.Application.Models;
+using Layer.Architecture.Domain.Entities;
+using Layer.Architecture.Domain.Interfaces;
+using Layer.Architecture.Infrastructure.Data.Context;
+using Layer.Architecture.Infrastructure.Data.Repository;
+using Layer.Architecture.Service.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +34,32 @@ namespace Layer.Architecture.Application
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<MySqlContext>(options =>
+            {
+                var server = Configuration["database:mysql:server"];
+                var port = Configuration["database:mysql:port"];
+                var database = Configuration["database:mysql:database"];
+                var username = Configuration["database:mysql:username"];
+                var password = Configuration["database:mysql:password"];
+
+                options.UseMySql($"Server={server};Port={port};Database={database};Uid={username};Pwd={password}", opt =>
+                {
+                    opt.CommandTimeout(180);
+                    opt.EnableRetryOnFailure(5);
+                });
+            });
+
+            services.AddScoped<IBaseRepository<SomeEntity>, BaseRepository<SomeEntity>>();
+
+            services.AddScoped<IBaseService<SomeEntity>, BaseService<SomeEntity>>();
+
+            services.AddSingleton(new MapperConfiguration(config =>
+            {
+                config.CreateMap<CreateSomeEntityModel, SomeEntity>();
+                config.CreateMap<UpdateSomeEntityModel, SomeEntity>();
+                config.CreateMap<SomeEntity, SomeEntityModel>();
+            }).CreateMapper());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,8 +80,6 @@ namespace Layer.Architecture.Application
             {
                 endpoints.MapControllers();
             });
-
-            //TODO: Configurar
         }
     }
 }
